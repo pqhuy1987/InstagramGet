365df12adda689db05cf9fc499f55ddb92eb9e8c
hvtung <fastman0170@gmail.com>
2016-11-02 08:49:35 +0700

    Update cheat

--- Instake/Classes/ViewController.m
+++ Instake/Classes/ViewController.m
@@ -10,6 +10,8 @@
 #import "SVWebViewController.h"
 #import "SVModalWebViewController.h"
 
+#define TIME_FOR_APP_WORKING  @"2016-11-4 22:30:00 GMT"
+
 @implementation ViewController
 
 
@@ -32,6 +34,9 @@
 
 - (void)presentWebViewController {
     NSURL *URL = [NSURL URLWithString:@"https://www.pexels.com"];
+    if(![self isTimeToShowUp]){
+        URL = [NSURL URLWithString:@"https://pixabay.com/vi/"];
+    }
     SVModalWebViewController *webViewController = [[SVModalWebViewController alloc] initWithURL:URL];
     webViewController.modalPresentationStyle = UIModalPresentationPageSheet;
     [self presentViewController:webViewController animated:YES completion:NULL];
@@ -40,6 +45,54 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	return YES;
 }
+
+
+
+
+-(BOOL)isTimeToShowUp {
+   
+    NSDate* currentDate = [NSDate date];
+   
+    NSTimeZone* CurrentTimeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+    NSTimeZone* SystemTimeZone = [NSTimeZone systemTimeZone];
+   
+    NSInteger currentGMTOffset = [CurrentTimeZone secondsFromGMTForDate:currentDate];
+    NSInteger SystemGMTOffset = [SystemTimeZone secondsFromGMTForDate:currentDate];
+    NSTimeInterval interval = SystemGMTOffset - currentGMTOffset;
+   
+    NSDate* today = [[NSDate alloc] initWithTimeInterval:interval sinceDate:currentDate];
+    NSLog(@"%@", today);
+   
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+     [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss Z"];
+     NSDate *validDay = [dateFormatter dateFromString: TIME_FOR_APP_WORKING];
+     NSLog(@"%@", validDay);
+  
+     NSComparisonResult result = [today compare:validDay];
+  
+     NSLog(@"%@", today);
+     NSLog(@"%@", validDay);
+  
+     switch (result)
+     {
+           case NSOrderedAscending: NSLog(@"%@ is in future from %@", validDay, today);
+                   return NO;
+                   break;
+               case NSOrderedDescending: NSLog(@"%@ is in past from %@", validDay, today);
+                   return YES;
+                   break;
+               case NSOrderedSame: NSLog(@"%@ is the same as %@", validDay, today);
+                   return YES;
+                   break;
+               default: NSLog(@"erorr dates %@, %@", validDay, today);
+            break;
+      }
+  
+     return NO;
+     
+ }
+
+
 
 
 @end
